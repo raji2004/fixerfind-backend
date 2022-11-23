4a0f839a8329f5e141e04919e4ee02d5dedaab29
raji2004 <kingraj1344@gmail.com>
2022-11-23 08:15:55 +0100

    s

--- helpers/validation.js
+++ helpers/validation.js
@@ -24,7 +24,7 @@ exports.Mailer = async (name, email, pin) => {
     from: process.env.GMAIL_USER,
     to: email,
     subject: `Registration Code`,
-    html: `html lang="en">
+    html: `<html lang="en">
     <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
