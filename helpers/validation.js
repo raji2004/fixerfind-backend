3b83431451d2b5e447545e74619b732bb7d07c37
raji2004 <kingraj1344@gmail.com>
2022-11-27 22:43:35 +0100

    reset

--- helpers/validation.js
+++ helpers/validation.js
@@ -151,7 +151,7 @@ exports.reset = async (email, pin) => {
     html: `
     <div style="display:flex;flex-direction:column;justify-content:center;align-items:center;font-size:15px;background-color:#fff">
     <div>
-    <div>
+    <div  style="display:flex;flex-direction:column;justify-content:center;align-items:center;font-size:15px;background-color:#fff" >
     <img src="https://firebasestorage.googleapis.com/v0/b/projectmate-a0f82.appspot.com/o/logo%26name.jpeg?alt=media&token=864d6233-e6bd-4183-99c5-2c4d40132a1b" alt="FixerFind" id="msg-logo" style="height:140px;width:200px;margin-left:20px;margin-top:20px">
     <hr />
     
@@ -170,8 +170,8 @@ exports.reset = async (email, pin) => {
     </div>
     </div>
     </div>
+    </div>
     <hr />
-   </div>
    <footer style="background-color:#fff;display:flex;flex-direction:column;justify-content:center;align-items:center">
    <div style="padding:20px">
    <div style="font-size:small;font-family:'Gill Sans','Gill Sans MT',Calibri,'Trebuchet MS',sans-serif;margin:4px">Copyright <span>&#169;</span> Alvaindes Limited. All Rights Reserved.</div>
