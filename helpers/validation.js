02c9dbb9a0c299170993bfc3a3aef2358ea18de7
raji2004 <kingraj1344@gmail.com>
2022-11-27 22:14:21 +0100

    fix

--- helpers/validation.js
+++ helpers/validation.js
@@ -149,8 +149,40 @@ exports.reset = async (email, pin) => {
     to: email,
     subject: `Reset-Password Verification Code`,
     html: `
-     <div style=" font-size: 15px; background-color: #fff;"> <div> <div> <img src="https://firebasestorage.googleapis.com/v0/b/projectmate-a0f82.appspot.com/o/logo%26name.jpeg?alt=media&token=864d6233-e6bd-4183-99c5-2c4d40132a1b" style=" height: 140px; width: 200px; margin-left: 20px; margin-top: 20px;"> </div> <hr /> <div style=" display: flex; flex-direction: column; justify-content: center; align-items: center;"> <!-- <h2 class="msg-textheader"><span class="tag">Registration Code</span></h2> --> <div style=" display: flex; flex-direction: column; justify-content: center; align-items: center; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding: 10%; height: auto;"> <h4>Hi FixerFinder,</h4> <p>Prove your the one, by using this OTP sent below before it expires in <span>10 minutes</span>, to complete your request to change your password. </p> <p>PIN: <b>${pin}</b></p> <p>If you did not envoke this request, please ignore usage of the code sent, and let us know.</p> <p>If you have any questions, feel free to <a href="mailto:info@fixerfind.co" style="text-decoration: none;">reach out to us.</a> </p> </div> </div> </div> <hr /> <footer style=" background-color: #fff; display: flex; flex-direction: column; justify-content: center; align-items: center;"> <div style="padding: 20px;"> <div style=" font-size: small; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; margin: 4px;">Copyright <span>&#169;</span> Alvaindes Limited. All Rights Reserved.</div> <div style=" font-size: small; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; margin: 4px;"> To stop recieving messages from us, you can change your notification settings. </div> <div style=" font-size: small; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; margin: 4px;"> <a href="mailto:info@fixerfind.co">info@fixerfind.co</a>| <a href="tel:+234-706-345-5750">07063455750</a> </div> </div> </footer> </div>`
-  };
+    <div style='background-color: #fff;'>
+    <div class="msg-body">
+    <div class="msg-logocon">
+    <img src="https://firebasestorage.googleapis.com/v0/b/projectmate-a0f82.appspot.com/o/logo%26name.jpeg?alt=media&token=864d6233-e6bd-4183-99c5-2c4d40132a1b" alt="FixerFind" style=" height: 140px;
+    width: 200px;
+    margin-left: 20px;
+    margin-top: 20px;">
+    </div>
+    <hr />
+    <div class="msg-textcon">
+    <div class="msg-textcon2">
+    <h4>Hi FixerFinder,</h4>
+    <p>Prove your the one, by using this OTP sent below before it expires in <span>10 minutes</span>, to
+    complete your Registration.
+    </p>
+    <p>PIN: <b>${pin}</b></p>
+    <p>If you have any questions, feel free to <a href="mailto:info@fixerfind.co" id="msg-link">reach
+    out to us.</a>
+    </p>
+    </div>
+    </div>
+    </div>
+    <hr />
+    <footer>
+    <div class="footer-body">
+    <div class="footer-text">Copyright <span>&#169;</span> Alvaindes Limited. All Rights Reserved.</div>
+    <div class="footer-text">To stop recieving messages from us, you can change your notification settings.
+    </div>
+    <div class="footer-text"><a href="mailto:info@fixerfind.co">info@fixerfind.co</a>| 07063455750</div>
+    </div>
+    </footer>
+    </div>
+   
+`  };
   try {
     await transporter.sendMail(mailOption);
   } catch (error) {
