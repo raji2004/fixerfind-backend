e66233e0e425aa2c22d5e84f990ffcd0081acd54
raji2004 <kingraj1344@gmail.com>
2022-11-27 22:51:06 +0100

    reset

--- helpers/validation.js
+++ helpers/validation.js
@@ -155,8 +155,8 @@ exports.reset = async (email, pin) => {
     <img src="https://firebasestorage.googleapis.com/v0/b/projectmate-a0f82.appspot.com/o/logo%26name.jpeg?alt=media&token=864d6233-e6bd-4183-99c5-2c4d40132a1b" alt="FixerFind" id="msg-logo" style="height:140px;width:200px;margin-left:20px;margin-top:20px">
     <hr />
     
-    <div style="display:flex;justify-content:center;align-items:center;
-    padding:10%;height:auto">
+    <div style="
+    padding:50%;height:auto">
     <h4>Hi FixerFinder,</h4>
     <p>Prove your the one, by using this OTP sent below before it expires in <span>10 minutes</span>, to
     complete your request to change your password.
