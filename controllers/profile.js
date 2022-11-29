75b2651b085ca782f322cfc12a3b6e3d52c4ea1d
raji2004 <kingraj1344@gmail.com>
2022-11-29 18:57:06 +0100

    profile done

--- controllers/profile.js
+++ controllers/profile.js
@@ -16,7 +16,8 @@ exports.changeprofile = async(req,res)=>{
   const {id,Firstname,Lastname,email,phone_no,location,description,socials} = req.body
   const data = {Firstname,Lastname,email,phone_no,location,description,socials}
   const user = await User.findOneAndUpdate({id},data)
-
+  user?res.send(user):res.status(404).json({message:"user not found"})
+ 
 
 };
 
