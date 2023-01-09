87c0b803473980527527728dc89c5657f9ca6464
raji2004 <kingraj1344@gmail.com>
2023-01-09 13:01:26 +0100

    delete

--- controllers/user.js
+++ controllers/user.js
@@ -122,7 +122,7 @@ exports.login = async (req, res) => {
     let { verified, id, deleted, deletedtime } = user;
     const todays = new Date()
     const timeRequested = new Date(deletedtime)
-    if (has31DaysPassed(timeRequested, todays)) {
+    if (!has31DaysPassed(timeRequested, todays)) {
       deleted = true
       User.findOneAndUpdate({ id }, { $unset: { deletedtime }, deleted })
     }else{
